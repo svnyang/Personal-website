897b5ba4b6e90475d9938894d96aba8c8b2c0ae2
SunYang <38999312+svnyang@users.noreply.github.com>
2023-11-16 11:19:49 +0800

    Add files via upload

---
+++ js/Valine.js
@@ -0,0 +1,6 @@
+document.write('<script src="https://unpkg.com/valine@1.5.0/dist/Valine.min.js"></script>');
+document.write('<script src="js/bootstrap.bundle.min.js" type="text/javascript" charset="utf-8"></script>');
+document.write('<script src="js/jquery.min.js" type="text/javascript" charset="utf-8"></script>');
+document.write('<script src="js/clipboard.min.js" type="text/javascript" charset="utf-8"></script>');
+document.write('<script src="js/EasyLazyload.min.js" type="text/javascript" charset="utf-8"></script>');
+document.write('<script src="js/details.js?=3" type="text/javascript" charset="utf-8"></script>');
